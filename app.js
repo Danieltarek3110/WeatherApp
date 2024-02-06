1bdd105215c9e85f1b4830bc30801ad9c1e1f054
Daniel <76216668+Danieltarek3110@users.noreply.github.com>
2024-02-07 01:28:00 +0200

    Fixed bugs in project setup

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 //Importing packages
-const request = require('request');
+const request = require('postman-request');
 const chalk = require('chalk');
 const express = require('express');
+
+//
+const url = 'http://api.weatherapi.com/v1/current.json?key=83ea0460bee84aae82c224123240602&q=London';
+request({url: url} , (error , response)=>{
+    console.log(response);
+});
