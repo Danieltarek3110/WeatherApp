ce364c9a62ea8b3a46722604bafd5e20e5083351
Daniel <76216668+Danieltarek3110@users.noreply.github.com>
2024-02-12 01:14:34 +0200

    Destructuring objects and use the command line to provide location

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 // Importing packages
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const geocode = require('./utils/geocode');
 const weather = require('./utils/weather');
 
+const address = process.argv[2];
+
 // Use geocode with a callback
-geocode('Cairo', (error, geoData) => {
-    if (error) {
-        console.log(error);
-    } else {
-        
-        // Call weather with the obtained information from geocode
-        weather(geoData, (error, weatherData) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(chalk.bold.blue('Name:', geoData.Name));
-                console.log(chalk.bold.blue('Temperature:', weatherData.temp, '°C'));
-                console.log(chalk.bold.blue('Humidity:', weatherData.humidity));
-            }
-        });
-    }
-});
+if(!address){
+    console.log(chalk.red('Please provide an address'))
+}else{
+    geocode(address, (error, {Name , lat , lon}={}) => {
+        if (error) {
+            console.log(error);
+        } else {
+            
+            // Call weather with the obtained information from geocode
+            weather({lat ,lon}, (error, weatherData) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(chalk.bold.blue('Name:', Name));
+                    console.log(chalk.bold.blue('Temperature:', weatherData.temp, '°C'));
+                    console.log(chalk.bold.blue('Humidity:', weatherData.humidity));
+                }
+            });
+        }
+    });
+    
+
+}
+
+
